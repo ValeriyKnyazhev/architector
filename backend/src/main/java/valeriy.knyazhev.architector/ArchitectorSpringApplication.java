813384297342af49162e54f540f7e1f4e5b87194
Valeriy Knyazhev <valeriy.knyazhev@yandex.ru>
2018-07-21 17:12:20 +0300

    setup building as multi project

---
+++ backend/src/main/java/valeriy.knyazhev.architector/ArchitectorSpringApplication.java
@@ -0,0 +1,18 @@
+package valeriy.knyazhev.architector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author valeriy.knyazhev@yandex.ru
+ */
+@SpringBootApplication
+public class ArchitectorSpringApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ArchitectorSpringApplication.class, args);
+    }
+
+}
