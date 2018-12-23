7d7c698df906f243113c4b6e9865017579edf9cd
Valeriy Knyazhev <valeriy.knyazhev@yandex.ru>
2018-12-23 21:06:26 +0300

    tests updated

--- backend/src/test/java/valeriy.knyazhev.architector/port/adapter/project/file/FileResourceTests.java
+++ backend/src/test/java/valeriy.knyazhev.architector/port/adapter/project/file/FileResourceTests.java
@@ -1,4 +1,4 @@
-package valeriy.knyazhev.architector.port.adapter.project;
+package valeriy.knyazhev.architector.port.adapter.project.file;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,10 +12,15 @@ import valeriy.knyazhev.architector.application.IFCFileReader;
 import valeriy.knyazhev.architector.domain.model.project.Project;
 import valeriy.knyazhev.architector.domain.model.project.ProjectId;
 import valeriy.knyazhev.architector.domain.model.project.ProjectRepository;
+import valeriy.knyazhev.architector.domain.model.project.file.File;
+import valeriy.knyazhev.architector.domain.model.project.file.FileId;
+import valeriy.knyazhev.architector.port.adapter.project.AddFileCommand;
+import valeriy.knyazhev.architector.port.adapter.project.ProjectResource;
 
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -26,11 +31,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * @author Valeriy Knyazhev <valeriy.knyazhev@yandex.ru>
  */
 @RunWith(SpringRunner.class)
-@WebMvcTest(ProjectResource.class)
-public class ProjectResourceTests {
+@WebMvcTest(FileResource.class)
+public class FileResourceTests {
 
     @MockBean
-    private IFCFileReader projectReader;
+    private IFCFileReader fileReader;
 
     @MockBean
     private ProjectRepository projectRepository;
@@ -45,29 +50,26 @@ public class ProjectResourceTests {
     }
 
     @Test
-    public void shouldCreateProject()
+    public void shouldAddFile()
             throws Exception {
+        // given
+        String fileUrl = "https://test.projects.ru/example.ifc";
+        String createCommand = "{\"fileUrl\": \"" + fileUrl + "\"}";
+        AddFileCommand expectedCommand = new AddFileCommand();
+        expectedCommand.setFileUrl(fileUrl);
+        ProjectId projectId = ProjectId.nextId();
+        Project project = sampleProject(projectId);
+        File file = File.builder().fileId(FileId.nextId()).build();
+        when(this.projectRepository.findByProjectId(any())).thenReturn(Optional.of(project));
+        when(this.fileReader.readFileFromUrl(any())).thenReturn(file);
+
         // expect
-        this.mockMvc.perform(post("/projects")
+        this.mockMvc.perform(post("/projects/{projectId}/files", projectId.id())
+                .content(createCommand)
                 .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.info").exists());
-        verify(this.projectRepository, times(1)).save(any(Project.class));
-    }
-
-    @Test
-    public void shouldReturnProject()
-            throws Exception {
-        // given
-        ProjectId projectId = ProjectId.nextId();
-        when(this.projectRepository.findByProjectId(eq(projectId))).thenReturn(Optional.of(sampleProject(projectId)));
-
-        // expect
-        this.mockMvc.perform(get("/projects/{projectId}", projectId.id())
-                .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.projectId").value(projectId.id()))
-                .andExpect(jsonPath("$.files").exists());
+        verify(this.projectRepository, times(1)).save(project);
     }
 
 }
