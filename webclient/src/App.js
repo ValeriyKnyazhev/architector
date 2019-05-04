b2e1e617b1ac67d8167e91d773328eae6d28e2c9
Valeriy Knyazhev <valeriy.knyazhev@yandex.ru>
2019-05-04 16:48:19 -0400

    remove Link and add descriptions

--- webclient/src/App.js
+++ webclient/src/App.js
@@ -1,27 +1,28 @@
-import React, { Component } from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect
-} from "react-router-dom";
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import ProjectsList from './pages/ProjectsList';
 import Project from './pages/Project/';
 import File from './pages/Project/File';
-import "./App.css";
+import './App.css';
 
 class App extends Component {
   render() {
-    return <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/projects" component={ProjectsList} />
-          <Route path="/projects/:projectId" component={Project} />
-          <Route path="/files/:fileId" component={File} />
-          <Redirect exact from="/" to="/projects" />
-        </Switch>
-      </Router>
-    </div>;
+    return (
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route
+              exact
+              path="/projects"
+              render={({ history }) => <ProjectsList history={history} />}
+            />
+            <Route path="/projects/:projectId" component={Project} />
+            <Route path="/files/:fileId" component={File} />
+            <Redirect exact from="/" to="/projects" />
+          </Switch>
+        </Router>
+      </div>
+    );
   }
 }
 
--- webclient/src/pages/ProjectsList/index.js
+++ webclient/src/pages/ProjectsList/index.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import axios from 'axios';
 import _isEmpty from 'lodash/isEmpty';
 import { Button, Icon, Input, message, Modal, Popconfirm, Pagination } from 'antd';
 import dayjs from 'dayjs';
 import './ProjectsList.sass';
 
+const { TextArea } = Input;
 export default class Projects extends Component {
   state = {
     projects: [],
@@ -101,6 +101,7 @@ export default class Projects extends Component {
 
   render() {
     const { projects, confirmLoading, visibleCreateProject } = this.state;
+    const { history } = this.props;
     return (
       <div className="container">
         <div>
@@ -122,6 +123,7 @@ export default class Projects extends Component {
               }}
             >
               <div style={{ marginBottom: 16 }}>
+                <div className="projects__input-label">Name</div>
                 <Input
                   placeholder="Enter your project name"
                   value={this.state.newProjectData.name}
@@ -130,7 +132,8 @@ export default class Projects extends Component {
                 />
               </div>
               <div style={{ marginBottom: 16 }}>
-                <Input
+                <div className="projects__input-label">Description</div>
+                <TextArea
                   placeholder="Enter your project description"
                   value={this.state.newProjectData.description}
                   onChange={this.onChangeProjectDescription}
@@ -155,38 +158,55 @@ export default class Projects extends Component {
                 this.state.pageSize * (this.state.currentPage - 1),
                 this.state.pageSize * this.state.currentPage
               )
-              .map(({ projectId, createdDate, updatedDate, projectName, author, files }) => (
-                <div className="col-xs-12 col-sm-6" key={projectId}>
-                  <Link
-                    to={{
-                      pathname: `/projects/${projectId}`,
-                      state: { projectId }
-                    }}
-                  >
-                    <div className="projects__project" key={projectId}>
+              .map(
+                ({
+                  projectId,
+                  createdDate,
+                  updatedDate,
+                  projectName,
+                  author,
+                  files,
+                  description
+                }) => (
+                  <div className="col-xs-12 col-sm-6" key={projectId}>
+                    <div
+                      className="projects__project"
+                      onClick={() => history.push(`/projects/${projectId}`)}
+                    >
                       <div className="row projects__project-name">
                         <div className="col-xs-9">{projectName}</div>
                       </div>
-                      <div className="row projects__project-date">
-                        <div className="col-xs-3">Created:</div>
-                        <div className="col-xs-9">{dayjs(createdDate).format('YYYY-MM-DD')}</div>
+                      <div className="row projects__project-descr">
+                        <div className="col-xs-9">{description}</div>
                       </div>
-                      <div className="row projects__project-date">
-                        <div className="col-xs-3">Updated:</div>
-                        <div className="col-xs-9">{dayjs(updatedDate).format('YYYY-MM-DD')}</div>
+                      <div className="projects__fields">
+                        <div className="projects__project-field">
+                          <div className="projects__field-name">Created:</div>
+                          <div className="projects__field-value">
+                            {dayjs(createdDate).format('YYYY-MM-DD')}
+                          </div>
+                        </div>
+                        <div className="projects__project-field">
+                          <div className="projects__field-name">Updated:</div>
+                          <div className="projects__field-value">
+                            {dayjs(updatedDate).format('YYYY-MM-DD')}
+                          </div>
+                        </div>
                       </div>
-                      <div className="row projects__project-author">
-                        <div className="col-xs-3">Author:</div>
-                        <div className="col-xs-9">{author}</div>
+                      <div className="projects__fields">
+                        <div className="projects__project-field">
+                          <div className="projects__field-name">Author:</div>
+                          <div className="projects__field-value">{author}</div>
+                        </div>
                       </div>
                       <div className="row projects__project-files">
                         <div className="col-xs-3">Files</div>
                         <div className="col-xs-9">{files.length}</div>
                       </div>
                     </div>
-                  </Link>
-                </div>
-              ))}
+                  </div>
+                )
+              )}
           </div>
         )}
         <Pagination
