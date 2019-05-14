76ace7eeb228113d19a3172f4ec6f370322c9984
Valeriy Knyazhev <valeriy.knyazhev@yandex.ru>
2019-05-14 03:26:38 +0300

    add file changes statistics

--- backend/src/main/java/valeriy.knyazhev.architector/application/commit/ChangesApplicationService.java
+++ backend/src/main/java/valeriy.knyazhev.architector/application/commit/ChangesApplicationService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import valeriy.knyazhev.architector.application.commit.command.FetchCommitChangesCommand;
 import valeriy.knyazhev.architector.application.commit.data.changes.*;
+import valeriy.knyazhev.architector.application.commit.data.changes.FileChangesData.FileChangesStatistics;
 import valeriy.knyazhev.architector.domain.model.commit.*;
 import valeriy.knyazhev.architector.domain.model.commit.projection.Projection;
 import valeriy.knyazhev.architector.domain.model.commit.projection.Projection.FileProjection;
@@ -19,6 +20,7 @@ import java.util.stream.Collectors;
 
 import static valeriy.knyazhev.architector.application.commit.data.changes.ChangedValue.changeValue;
 import static valeriy.knyazhev.architector.application.commit.data.changes.ChangedValue.newValue;
+import static valeriy.knyazhev.architector.domain.model.commit.ChangeType.ADDITION;
 
 /**
  * @author Valeriy Knyazhev <valeriy.knyazhev@yandex.ru>
@@ -89,6 +91,7 @@ public class ChangesApplicationService
         {
             return new FileChangesData(
                 changes.fileId().id(),
+                FileChangesStatistics.added(changes.items().size(), true),
                 constructFileMetadataChanges(null, changes.metadata()),
                 constructFileDescriptionChanges(null, changes.description()),
                 defineSections(Collections.emptyList(), changes.items())
@@ -100,8 +103,10 @@ public class ChangesApplicationService
         FileDescriptionChangesData descriptionChanges = constructFileDescriptionChanges(
             fileProjection.description(), changes.description()
         );
+        boolean characteristicsModified = !(changes.metadata().isEmpty() && changes.description().isEmpty());
         return new FileChangesData(
             changes.fileId().id(),
+            createChangesStatistics(fileProjection, changes.items(), characteristicsModified),
             metadataChanges,
             descriptionChanges,
             defineSections(fileProjection.items(), changes.items())
@@ -110,7 +115,7 @@ public class ChangesApplicationService
 
     @Nonnull
     // TODO improve this code
-    private FileMetadataChangesData constructFileMetadataChanges(
+    private static FileMetadataChangesData constructFileMetadataChanges(
         @Nullable FileMetadata metadata,
         @Nonnull FileMetadataChanges changes)
     {
@@ -129,8 +134,8 @@ public class ChangesApplicationService
         return FileMetadataChangesData.builder()
             .name(
                 changes.name() != null
-                    ? changeValue(metadata.name(), changes.name())
-                    : null
+                ? changeValue(metadata.name(), changes.name())
+                : null
             )
             .timestamp(
                 changes.timestamp() != null
@@ -139,35 +144,35 @@ public class ChangesApplicationService
             )
             .authors(
                 changes.authors() != null
-                    ? changeValue(metadata.authors(), changes.authors())
-                    : null
+                ? changeValue(metadata.authors(), changes.authors())
+                : null
             )
             .organizations(
                 changes.organizations() != null
-                    ? changeValue(metadata.organizations(), changes.organizations())
-                    : null
+                ? changeValue(metadata.organizations(), changes.organizations())
+                : null
             )
             .preprocessorVersion(
                 changes.preprocessorVersion() != null
-                    ? changeValue(metadata.preprocessorVersion(), changes.preprocessorVersion())
-                    : null
+                ? changeValue(metadata.preprocessorVersion(), changes.preprocessorVersion())
+                : null
             )
             .originatingSystem(
                 changes.originatingSystem() != null
-                    ? changeValue(metadata.originatingSystem(), changes
+                ? changeValue(metadata.originatingSystem(), changes
                     .originatingSystem()) : null
             )
             .authorization(
                 changes.authorization() != null
-                    ? changeValue(metadata.authorization(), changes.authorization())
-                    : null
+                ? changeValue(metadata.authorization(), changes.authorization())
+                : null
             )
             .build();
     }
 
     @Nonnull
     // TODO improve this code
-    private FileDescriptionChangesData constructFileDescriptionChanges(
+    private static FileDescriptionChangesData constructFileDescriptionChanges(
         @Nullable FileDescription description,
         @Nonnull FileDescriptionChanges changes)
     {
@@ -175,26 +180,46 @@ public class ChangesApplicationService
         {
             return FileDescriptionChangesData.builder()
                 .descriptions(newValue(changes.descriptions()))
-                .descriptions(newValue(changes.descriptions()))
+                .implementationLevel(newValue(changes.implementationLevel()))
                 .build();
         }
         return FileDescriptionChangesData.builder()
             .descriptions(
                 changes.descriptions() != null
-                    ? changeValue(description.descriptions(), changes.descriptions())
-                    : null
+                ? changeValue(description.descriptions(), changes.descriptions())
+                : null
             )
             .implementationLevel(
                 changes.implementationLevel() != null
-                    ? changeValue(description.implementationLevel(), changes.implementationLevel())
-                    : null
+                ? changeValue(description.implementationLevel(), changes.implementationLevel())
+                : null
             )
             .build();
     }
 
+    private static FileChangesStatistics createChangesStatistics(@Nonnull FileProjection projection,
+                                                                 @Nonnull List<CommitItem> contentChanges,
+                                                                 boolean characteristicsModified)
+    {
+        int contentProjectionSize = projection.items().size();
+        long addedLines = contentChanges.stream()
+            .filter(change -> ADDITION == change.type())
+            .count();
+        long deletedLines = contentChanges.size() - addedLines;
+        if (contentProjectionSize == addedLines && deletedLines == 0)
+        {
+            return FileChangesStatistics.added(addedLines, characteristicsModified);
+        }
+        if (contentProjectionSize == deletedLines && addedLines == 0)
+        {
+            return FileChangesStatistics.deleted(deletedLines, characteristicsModified);
+        }
+        return FileChangesStatistics.modified(addedLines, deletedLines, characteristicsModified);
+    }
+
     @Nonnull
-    private List<SectionChangesData> defineSections(@Nonnull List<String> items,
-                                                    @Nonnull List<CommitItem> changes)
+    private static List<SectionChangesData> defineSections(@Nonnull List<String> items,
+                                                           @Nonnull List<CommitItem> changes)
     {
         return SectionChangesExtractor.sectionsOf(items)
             .applyChanges(changes)
--- backend/src/main/java/valeriy.knyazhev.architector/application/commit/data/changes/FileChangesData.java
+++ backend/src/main/java/valeriy.knyazhev.architector/application/commit/data/changes/FileChangesData.java
@@ -19,6 +19,9 @@ public class FileChangesData
     private String fileId;
 
     @Nonnull
+    private FileChangesStatistics statistics;
+
+    @Nonnull
     private FileMetadataChangesData metadata;
 
     @Nonnull
@@ -28,14 +31,69 @@ public class FileChangesData
     private List<SectionChangesData> sections;
 
     public FileChangesData(@Nonnull String fileId,
+                           @Nonnull FileChangesStatistics statistics,
                            @Nonnull FileMetadataChangesData metadata,
                            @Nonnull FileDescriptionChangesData description,
                            @Nonnull List<SectionChangesData> sections)
     {
         this.fileId = Args.notNull(fileId, "File identifier is required.");
+        this.statistics = Args.notNull(statistics, "File changes statistics is required.");
         this.metadata = Args.notNull(metadata, "File metadata is required.");
         this.description = Args.notNull(description, "File description is required.");
         this.sections = Args.notNull(sections, "File sections are required.");
+    }
+
+    @JsonAutoDetect(fieldVisibility = ANY)
+    public static class FileChangesStatistics
+    {
+
+        @Nonnull
+        private FileModificationType type;
+
+        private long addedLines;
+
+        private long deletedLines;
+
+        private boolean characteristicsModified;
+
+        private FileChangesStatistics(@Nonnull FileModificationType type,
+                                      long addedLines,
+                                      long deletedLines,
+                                      boolean characteristicsModified)
+        {
+            this.type = Args.notNull(type, "File modification type is required.");
+            this.addedLines = Args.notNegative(addedLines, "Added lines count must not be negative.");
+            this.deletedLines = Args.notNegative(deletedLines, "Deleted lines count must not be negative.");
+            this.characteristicsModified = characteristicsModified;
+        }
+
+        public static FileChangesStatistics added(long count, boolean characteristicsModified)
+        {
+            return new FileChangesStatistics(FileModificationType.ADDED, count, 0, characteristicsModified);
+        }
+
+        public static FileChangesStatistics deleted(long count, boolean characteristicsModified)
+        {
+            return new FileChangesStatistics(FileModificationType.DELETED, 0, count, characteristicsModified);
+        }
+
+        public static FileChangesStatistics modified(long addedLines,
+                                                     long deletedLines,
+                                                     boolean characteristicsModified)
+        {
+            return new FileChangesStatistics(
+                FileModificationType.MODIFIED, addedLines, deletedLines, characteristicsModified
+            );
+        }
+
+        public enum FileModificationType
+        {
+
+            ADDED, DELETED, MODIFIED
+
+        }
+
+
     }
 
 }
