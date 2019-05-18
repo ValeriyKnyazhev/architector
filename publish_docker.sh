2a48350322e9ffd2e7c26e1d8bda3ec9224443bb
Valeriy Knyazhev <valeriy.knyazhev@yandex.ru>
2019-05-18 14:17:06 +0300

    fix publich docker image script

--- publish_docker.sh
+++ publish_docker.sh
@@ -8,18 +8,18 @@ IMAGE_NAME=valeriyknyazhev/architector:${version}
 LOGIN=valeriyknyazhev
 
 echo ">>> Building local image: ${DOCKER_NAME}"
-docker build -t DOCKER_NAME .
+docker build -t ${DOCKER_NAME} .
 
 echo ">>> Tagging image to new name: ${IMAGE_NAME}"
 docker tag ${DOCKER_NAME} ${IMAGE_NAME}
 
-echo ">>> Logging in to Docker Hub"
+echo ">>> Logging into Docker Hub"
 cat docker-credentials.conf | docker login --username ${LOGIN} --password-stdin
 
 echo ">>> Pushing image"
 docker push ${IMAGE_NAME}
 
-echo ">>> Image published on DockerHub"
+echo ">>> Image published into Docker Hub"
 
 echo ">>> Deleting local resources"
 docker rmi ${IMAGE_NAME}
