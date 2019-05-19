2f0e2e5f68339f2d7403068075da34cd8bfd55b3
Valeriy Knyazhev <valeriy.knyazhev@yandex.ru>
2019-05-19 15:28:10 -0400

    update diff metadata and descr

--- webclient/src/pages/Project/Commit/index.js
+++ webclient/src/pages/Project/Commit/index.js
@@ -34,6 +34,42 @@ function DiffRowComponent({ data }) {
   );
 }
 
+function DiffMetadataValue({ descr, data }) {
+  return (
+    <>
+      <div style={{ textAlign: 'left', margin: '8px 0 6px 0' }} className="around">
+        {descr}:
+      </div>
+      <div className="d-flex around">
+        <div className="commit__row--delete around">{data.oldValue}</div>
+        <div className="commit__row--add around">{data.newValue}</div>
+      </div>
+    </>
+  );
+}
+
+function DiffMetadataValues({ descr, data }) {
+  return (
+    <>
+      <div style={{ textAlign: 'left', margin: '8px 0 6px 0' }} className="around">
+        {descr}:
+      </div>
+      <div className="d-flex around">
+        <div className="commit__row--delete around">
+          {data.oldValue.map(value => (
+            <div className="start-xs">{value}</div>
+          ))}
+        </div>
+        <div className="commit__row--add around">
+          {data.newValue.map(value => (
+            <div className="start-xs">{value}</div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
 export default class Commit extends PureComponent {
   state = {
     changedFiles: []
@@ -65,24 +101,59 @@ export default class Commit extends PureComponent {
         <div>
           <h2>Commit #{commitId}</h2>
           {changedFiles.map(file => (
-            <div className="commit__file">
-              <div className="commit__file-info">
-                TYPE: {file.statistics.type}
-                <br />+ {file.statistics.addedLines} / - {file.statistics.deletedLines}
-              </div>
-              <div>
-                {file.sections.map(section => (
-                  <Fragment>
-                    <div className="commit__section">
-                      {section.items.map(item => (
-                        <DiffRowComponent data={item} />
-                      ))}
+            <>
+              <p style={{ textAlign: 'left' }}>File name: {file.name}</p>
+              <div className="commit__file">
+                <div className="commit__file-info">
+                  TYPE: {file.statistics.type}
+                  <br />
+                  {file.sections.lenght > 0 && (
+                    <div>
+                      {file.statistics.addedLines} / - {file.statistics.deletedLines}
                     </div>
-                    <Icon type="small-dash" />
-                  </Fragment>
-                ))}
+                  )}
+                </div>
+                {file.metadata.name !== null && (
+                  <DiffMetadataValue descr="Metadata Name" data={file.metadata.name} />
+                )}
+                {file.metadata.authors !== null && (
+                  <DiffMetadataValues descr="Metadata Authors" data={file.metadata.authors} />
+                )}
+                {file.metadata.organizations !== null && (
+                  <DiffMetadataValues descr="Metadata Authors" data={file.metadata.organizations} />
+                )}
+                {file.metadata.preprocessorVersion !== null && (
+                  <DiffMetadataValue
+                    descr="Metadata Preprocessor version"
+                    data={file.metadata.preprocessorVersion}
+                  />
+                )}
+                {file.metadata.originatingSystem !== null && (
+                  <DiffMetadataValue
+                    descr="Metadata Originating System"
+                    data={file.metadata.originatingSystem}
+                  />
+                )}
+                {file.description.descriptions !== null && (
+                  <DiffMetadataValues
+                    descr="Metadata Authors"
+                    data={file.description.descriptions}
+                  />
+                )}
+                <div>
+                  {file.sections.map(section => (
+                    <Fragment>
+                      <div className="commit__section">
+                        {section.items.map(item => (
+                          <DiffRowComponent data={item} />
+                        ))}
+                      </div>
+                      <Icon type="small-dash" />
+                    </Fragment>
+                  ))}
+                </div>
               </div>
-            </div>
+            </>
           ))}
         </div>
       </div>
