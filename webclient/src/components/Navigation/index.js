4308485aec132f31d5e35ae5dda612dd5f7c75e0
Valeriy Knyazhev <valeriy.knyazhev@yandex.ru>
2019-05-22 15:21:29 -0400

    fix header nav

--- webclient/src/components/Navigation/index.js
+++ webclient/src/components/Navigation/index.js
@@ -30,21 +30,25 @@ class Navigation extends Component {
 
     return (
       <header>
-        <div className="container architector">
-          <div className="row architector__header">
-            <Link to="/projects">
-              <h3 className="architector__header-title start-xs">Architector</h3>
-            </Link>
-            <div className="architector__header-user col-xs-6">User: {email}</div>
-            <div className="col-xs-5 end-xs">
-              <Button
-                className="architector__header-logout "
-                href="/logout"
-                type="danger"
-                style={{ alignContent: 'right' }}
-              >
-                Logout <Icon type="logout" />
-              </Button>
+        <div className="architector">
+          <div className="container">
+            <div className="row architector__header">
+              <div className=" col-xs-1">
+                <Link to="/projects">
+                  <div className="architector__header-title  start-xs">Architector</div>
+                </Link>
+              </div>
+              <div className="architector__header-user col-xs-6">User: {email}</div>
+              <div className="col-xs-5 end-xs">
+                <Button
+                  className="architector__header-logout "
+                  href="/logout"
+                  type="danger"
+                  style={{ alignContent: 'right' }}
+                >
+                  Logout <Icon type="logout" />
+                </Button>
+              </div>
             </div>
           </div>
         </div>
